a76fd401c27b415049ee6960e465bc8368729e41
LoSSpyro <wayneftl@web.de>
2016-05-19 10:45:02 +0200

    FIrst

---
+++ SideScroller/Assets/Scripts/GameController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn {
+	public Vector2 minSpawn { get; set; }
+	public Vector2 maxSpawn { get; set; }
+
+	public Spawn(float minX, float maxX, float minY, float maxY) {
+		minSpawn.Set (minX, minY);
+		maxSpawn.Set (maxX, maxY);
+	}
+
+	public Spawn() {
+		minSpawn.Set (0f, 0f);
+		maxSpawn.Set (0f, 0f);
+	}
+
+	public float RandomX () {
+		return Random.Range(minSpawn.x, maxSpawn.x);
+	}
+
+	public float RandomY () {
+		return Random.Range (minSpawn.y, maxSpawn.y);
+	}
+
+	public Vector3 RandomSpawnPosition () {
+		return new Vector3 (RandomX (), RandomY());
+	}
+
+	public string ToString() {
+		return "Min: (" + minSpawn.x + ", " + minSpawn.y + ") Max: (" + maxSpawn.x + ", " + maxSpawn.y + ")";
+	}
+}
+
+public class GameController : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject pickUp;
+	public GameObject platform;
+
+	private PlayerMovement playerMovement;
+
+	public int spawnCounter = 3;
+	public float downHeightDifference;
+	public float upHeightDifference;
+	public float minDistance;
+	public float maxDistance;
+	public float deadZone;
+
+
+	private Spawn platformSpawn = new Spawn ();
+	private Spawn pickUpSpawn = new Spawn();
+	private Vector3 originPosition = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		playerMovement = player.GetComponent<PlayerMovement> ();
+		platformSpawn.maxSpawn = new Vector2 (maxDistance, upHeightDifference);
+		platformSpawn.minSpawn = new Vector2 (minDistance, downHeightDifference);
+
+		pickUpSpawn.minSpawn = new Vector2 (-7.5f, 0.5f);
+		pickUpSpawn.maxSpawn = new Vector2 (0.5f, playerMovement.jumpPower);
+		Debug.Log (pickUpSpawn.ToString ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.transform.position.x >= originPosition.x - spawnCounter * 7.5f) {
+			Vector3 randomPosition = originPosition + platformSpawn.RandomSpawnPosition () + Vector3.right * 7.5f;
+			if (randomPosition.y <= deadZone + 5)
+				randomPosition.y = originPosition.y + platformSpawn.RandomY ();
+			Instantiate (platform, randomPosition, Quaternion.Euler (Vector3.up * 90f));
+
+			while(Random.value > 0.5f) {
+				Vector3 pickUpLocation = pickUpSpawn.RandomSpawnPosition ();
+				Debug.Log (pickUpLocation);
+				Instantiate (pickUp, randomPosition + pickUpLocation, Quaternion.identity);
+			}
+
+			originPosition = randomPosition;
+		}
+
+		if (player.transform.position.y < deadZone) {
+			player.SendMessage ("Restart");
+		}
+	}
+}
---
+++ SideScroller/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	public float maxSpeed;
+	public float jumpPower;
+	public float pickUpLasttime;
+
+	private bool grounded = true;
+	private bool jump = false;
+
+	private Vector3 startPosition;
+	private List<GameObject> collectedPickups = new List<GameObject>();
+
+	[HideInInspector]
+	public float collectionSpeed = 0f;
+	[HideInInspector]
+	public float jumpRange = 0f;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		startPosition = transform.position;
+
+	}
+
+	void Update() {
+		if (Input.GetKey (KeyCode.UpArrow) && grounded)
+			jump = true;
+
+		collectionSpeed += Time.deltaTime;
+	}
+
+	void FixedUpdate () {
+		float h = Input.GetAxis ("Horizontal");
+
+		if (h * rb.velocity.x < maxSpeed)
+			rb.AddForce (Vector3.right * h * speed);
+
+		if (Mathf.Abs (rb.velocity.x) > maxSpeed)
+			rb.velocity = new Vector3 (Mathf.Sign (rb.velocity.x) * maxSpeed, rb.velocity.y);
+
+		if (jump) {
+			rb.velocity += (Vector3.up * Mathf.Sqrt(-2f * Physics.gravity.y * jumpPower));
+			jump = false;
+		}
+		
+	}
+
+	void OnCollisionEnter (Collision collision) {
+			grounded = true;
+		jumpRange = transform.position.x - jumpRange;
+		Debug.Log ("Jump Range = " + jumpRange);
+	}
+
+	void OnCollisionExit (Collision collision) {
+		grounded = false;
+		jumpRange = transform.position.x;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		other.gameObject.SetActive (false);
+		collectionSpeed = 0f;
+		collectedPickups.Add (other.gameObject);
+		StartCoroutine (IncreaseMoveSpeed());
+	}
+
+	IEnumerator IncreaseMoveSpeed() {
+		maxSpeed++;
+		yield return new WaitForSeconds (pickUpLasttime);
+		maxSpeed--;
+	}
+
+	public void Restart() {
+		rb.velocity = Vector3.zero;
+		rb.angularVelocity = Vector3.zero;
+		transform.position = new Vector3 (-5f, 0.5f, 0f);
+		foreach (GameObject pickUp in collectedPickups) {
+			pickUp.SetActive (true);
+		}
+
+	}
+}
